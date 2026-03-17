d87dfd6f8da76df3e8befd223ac710aee722c2c2
aatmunbaxi <abaxi@tamu.edu>
2026-03-17 13:22:42 -0500

    Add flake for nix build

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Hugo build shell";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+
+  outputs = inputs:
+
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+        };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+           buildInputs = [
+              pkgs.hugo
+            ];
+        };
+      });
+    };
+}
